77680b79f98322a7af1a0cbc41b40302b4fc5c76
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2022-12-02 11:25:10 -0500

    Day 2

--- Main.lean
+++ Main.lean
@@ -1,7 +1,16 @@
 import Aoc2022
 
-def main : IO Unit := do
-  IO.println "Day1:"
-  IO.println s!"Part 1: {← Day1.first_part}"
-  IO.println s!"Part 2: {← Day1.second_part}"
-  IO.println ""
+def main (args : List String) : IO Unit :=
+  match args with
+  | ["1"] => do
+    IO.println "Day 1:"
+    IO.println s!"Part 1: {← Day1.first_part}"
+    IO.println s!"Part 2: {← Day1.second_part}"
+    IO.println ""
+  | ["2"] => do
+    IO.println "Day 2:"
+    IO.println s!"Part 1: {← Day2.first_part}"
+    IO.println s!"Part 2: {← Day2.second_part}"
+    IO.println ""
+  | _ => do
+    IO.println "Help, what should I do!?"
