4fe2232e0096232cbbaa625d24066aa015bdb4d7
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2022-12-09 13:11:21 -0500

    switch to RBSet

--- Aoc2022/Day09.lean
+++ Aoc2022/Day09.lean
@@ -1,12 +1,21 @@
 import Aoc2022.Utils
+import Std.Data.RBMap
 
-open System
+open System Std
+
+abbrev Pos := Int × Int
+instance : LT Pos := ⟨Prod.lexLt⟩
+
+instance : DecidableRel (α:=Pos) (· < ·) := fun a b => Prod.lexLtDec a b
+
+instance : Ord Pos := ⟨fun a b => compareOfLessAndEq a b⟩
 
 namespace Day9
 
 def input : FilePath := "/home/fred/lean/aoc2022/input_09"
 
-abbrev Pos := Int × Int
+
+
 
 /-
 PART 1:
@@ -49,19 +58,21 @@ def first_part : IO Nat := do
                                                       | [c, num] => (c, num.toNat!)
                                                       | _ => unreachable!)
                                      |>.push ("R", 1)
-  let mut poucet : List Pos := []
+  let mut poucet : RBSet Pos compare := RBSet.empty
   let mut headPos : Pos := ⟨0, 0⟩
   let mut tailPos : Pos := ⟨0, 0⟩
   for cmd in commands do
     let (dir, dist) := cmd
     for _ in [0:dist] do
-      match poucet.find? (· == tailPos) with
+      match poucet.find? tailPos with
       | some _ => headPos := moveHead headPos dir
                   tailPos := moveTail headPos tailPos
-      | none => poucet := tailPos :: poucet
+      | none => poucet := poucet.insert tailPos
                 headPos := moveHead headPos dir
                 tailPos := moveTail headPos tailPos
-  return poucet.length
+  return poucet.size
+
+#eval first_part
 
 def maxPos : IO String := do
   let commands := (← IO.FS.lines input).map String.splitOn
@@ -105,15 +116,15 @@ def second_part : IO Nat := do
                                                       | [c, num] => (c, num.toNat!)
                                                       | _ => unreachable!)
                                      |>.push ("R", 1)
-  let mut poucet : List Pos := []
+  let mut poucet : RBSet Pos compare := RBSet.empty
   let mut rope : Array Pos := Array.mkArray 10 ⟨0, 0⟩
   for cmd in commands do
     let (dir, dist) := cmd
     for _ in [0:dist] do
-      match poucet.find? (· == rope[9]!) with
+      match poucet.find? rope[9]! with
       | some _ => rope := moveRope rope dir
-      | none => poucet := rope[9]! :: poucet
+      | none => poucet := poucet.insert rope[9]!
                 rope := moveRope rope dir
-  return poucet.length
+  return poucet.size
 
 end Day9
