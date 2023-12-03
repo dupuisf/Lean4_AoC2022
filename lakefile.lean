822c760045ea428c640a2396ed9ee4ac933230c5
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2023-12-03 13:07:10 -0500

    toolchain bump

--- lakefile.lean
+++ lakefile.lean
@@ -1,21 +1,19 @@
 import Lake
 open Lake DSL
 
-package aoc2022 {
+package «aoc2022» where
   -- add package configuration options here
-}
 
 @[default_target]
-lean_lib Aoc2022 
+lean_lib «Aoc2022» where
 
 @[default_target]
-lean_exe aoc2022 {
+lean_exe aoc where
   root := `Main
-}
-
-@[default_target]
-lean_exe mwe {
-  root := `Aoc2022.Scratchpad
-}
+  -- Enables the use of the Lean interpreter by the executable (e.g.,
+  -- `runFrontend`) at the expense of increased binary size on Linux.
+  -- Remove this line if you do not need such functionality.
+  --supportInterpreter := true
 
 require std from git "https://github.com/leanprover/std4" @ "main"
+--require aesop from git "https://github.com/leanprover-community/aesop" @ "master"
